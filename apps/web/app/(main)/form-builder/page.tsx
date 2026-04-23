1025d5a2014fb21cc77e7d08c87c62dad1400dba
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2026-04-23 14:49:42 +0200

    Wrap form builder page in Suspense

--- apps/web/app/(main)/form-builder/page.tsx
+++ apps/web/app/(main)/form-builder/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import * as React from "react";
 import { FormBuilder } from "@/form-builder/components/form-builder";
 import { FormStudioShell } from "@/form-builder/components/form-studio-shell";
 
 export default function FormBuilderPage() {
   return (
-    <FormStudioShell>
-      <FormBuilder />
-    </FormStudioShell>
+    <React.Suspense fallback={null}>
+      <FormStudioShell>
+        <FormBuilder />
+      </FormStudioShell>
+    </React.Suspense>
   );
 }
